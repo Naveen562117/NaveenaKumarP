d0c5d7be41debf78e8cdb6fe2ef647f58e099b2b
Naveena Kumar P <ns562117@gmail.com>
2020-07-05 16:59:25 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ class Footer extends Component {
       <section className="footer container">
         <p>Find me on social media.</p>
         <a href= 'http://facebook.com/ns562117'><i className="fa fa-facebook-official fa--opacity"></i></a>
-        <a href= 'https://www.instagram.com/navi562117/'><i className="fa fa-instagram fa--opacity"></i></a>
+        <a href= 'https://www.instagram.com/thenightfuryrider/'><i className="fa fa-instagram fa--opacity"></i></a>
         <a href= 'https://github.com/Naveen562117/'><i className="fa fa-github fa--opacity"></i></a>
         <a href= 'https://twitter.com/navi_appu'><i className="fa fa-twitter fa--opacity"></i></a>
         <a href= 'https://www.linkedin.com/in/naveen-kumar-p-530859134/'><i className="fa fa-linkedin fa--opacity"></i></a> 
